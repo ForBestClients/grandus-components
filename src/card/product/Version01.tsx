8f1f1358282135d0d5e02e4eaf3c1bf44a3af935
Ivan Kopcik <ivan.kopcik@gmail.com>
2022-03-15 15:46:06 +0100

    product card v 01 (beta)

--- src/card/product/Version01.tsx
+++ src/card/product/Version01.tsx
@@ -1,9 +1,88 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
-const Version01 = ({}): JSX.Element => {
+type Price = {
+  currency?: string;
+  currencysymbol?: string;
+  price?: number;
+  priceFormatted?: string;
+};
+
+type Brand = {
+  name?: string;
+};
+
+type Options = {
+  imageNode?: ReactNode;
+  brandNode?: ReactNode;
+  priceNode?: ReactNode;
+  labelsNode?: ReactNode;
+};
+
+type Props = {
+  name?: string;
+  brand?: Brand;
+  price?: Price;
+  priceOriginal?: Price;
+  [key: string]: any;
+  linkRender: any;
+  options?: Options;
+  children?: ReactNode;
+};
+
+type LinkRendererType = (props: {
+  children: React.ReactNode;
+}) => React.ReactElement;
+
+type CardLinkProps = {
+  linkRender: LinkRendererType;
+  children: React.ReactNode;
+  [key: string]: any;
+};
+
+const CardLink = ({
+  linkRender,
+  children,
+}: CardLinkProps): React.ReactElement => {
+  return linkRender({ children });
+};
+
+const Version01 = ({
+  name,
+  price,
+  priceOriginal,
+  brand,
+  linkRender,
+  options,
+}: Props): JSX.Element => {
+  const isPriceOriginalVisible =
+    (price?.price ?? 0) < (priceOriginal?.price ?? 0);
   return (
-    <div className={'wrapper'} data-testid="example-test">
-      test component
+    <div data-testid="card-product-version-01">
+      <CardLink linkRender={linkRender}>
+        <section className={'image'}>
+          {options?.imageNode}
+          {options?.labelsNode ? options?.labelsNode : ''}
+        </section>
+      </CardLink>
+
+      <section className={'content'}>
+        <section className={'actions'}>
+          {options?.brandNode ? options?.brandNode : brand?.name}
+        </section>
+        <section className={'title'}>{name}</section>
+        <section className={'prices'}>
+          {options?.priceNode ? (
+            options?.priceNode
+          ) : (
+            <>
+              <span>{price?.priceFormatted}</span>
+              {isPriceOriginalVisible && (
+                <del>{priceOriginal?.priceFormatted}</del>
+              )}
+            </>
+          )}
+        </section>
+      </section>
     </div>
   );
 };
